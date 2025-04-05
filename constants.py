5409eade837230a3e4f491800f7ef5d253abd68c
Ambar Rizvi <brannstrom9911@gmail.com>
2025-04-05 12:38:39 +0530

    constants

---
+++ constants.py
@@ -0,0 +1,22 @@
+class Constant:
+    """
+    This class to hold constant values used in the application.
+    """
+    NYKAA_PRODUCT_PATTERNS = [
+            '/buy/',
+            '/product/',
+            '/p/',
+            '/products/',
+            '/item/'
+        ]
+    NYKAA_EXCLUDE_PATTERNS = [
+            '/category/',
+            '/brand/',
+            '/search',
+            '/wishlist',
+            '/cart',
+            '/account',
+            '/login',
+            '/c/',
+            '/lp/'
+        ]
